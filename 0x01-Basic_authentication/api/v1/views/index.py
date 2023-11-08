9f1444edf6b2d2fc1e8c869c58d5a1e9dbc27f8a
keith-kay <keithrhova@gmail.com>
2023-11-08 17:03:28 +0300

    new changes

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
     """ GET /api/v1/unauthorized
     Return:
@@ -14,13 +14,13 @@ def unauthorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
-  """ GET /api/v1/forbidden
-  Return:
-    - Return error
-  """
-  abort(403)
+    """ GET /api/v1/foridden
+    Return:
+      - Forbidden error:
+    """
+    abort(403)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
