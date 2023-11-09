b4abc68c959c4f295e8d2d1b9ec3dca9b68bab80
keith-kay <keithrhova@gmail.com>
2023-11-09 12:53:03 +0300

    task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,9 +5,22 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
     """Defines authentication system
     """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Manages paths
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path.rstrip('/')):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Handles authentication request
