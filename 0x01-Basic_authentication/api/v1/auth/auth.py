80cbc46e3d885b78ed4c156594c3f4cddb297d32
keith-kay <keithrhova@gmail.com>
2023-11-09 12:41:20 +0300

    new

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,28 +1,22 @@
 #!/usr/bin/env python3
-""""Define a class auth
+"""Define a class auth
 """
-
 
 from flask import request
 from typing import List, TypeVar
 
-
 class Auth():
-    """ defines authentication system
+    """Defines authentication system
     """
-    
-    #def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        
 
     def authorization_header(self, request=None) -> str:
-        """"Handles authentication request
+        """Handles authentication request
         """
         if request is None:
             return None
         header = request.headers.get('Authorization')
         return header if header else None
-    
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Handles authentication users
         """
